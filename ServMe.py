19a172aca771490829ac2ee4612149bc64853cb6
Mike Dank <Famicoman@gmail.com>
2012-02-25 07:21:30 +0100

    updated code for host

--- ServMe.py
+++ ServMe.py
@@ -4,7 +4,7 @@ import string
 
 #Make socket
 srvsock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-srvsock.bind( ("127.0.0.1", string.atoi(sys.argv[1])) )
+srvsock.bind( (sys.argv[1], string.atoi(sys.argv[2])) )
 srvsock.listen( 5 )
 clisock, (remhost, remport) = srvsock.accept()
 
